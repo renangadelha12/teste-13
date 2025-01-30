dee3031f5301490963fbbb8ad077f1fd3d9fe1bd
renangadelha12 <118527388+renangadelha12@users.noreply.github.com>
2025-01-30 10:19:57 -0300

    Update dash_steamlit.py
    v3

--- dash_steamlit.py
+++ dash_steamlit.py
@@ -45,12 +45,12 @@ st.markdown(
 #st.image("logo l.png", width=200)
 
 #parte de pegar os dados
-topo = ['Date', 'Time', 'Temperatura', 'Hi temp', 'Low Temp', 'Umidade', 'Dew Pt.', 'Velocidade do Vento', 'Wind Dir', 'Wind Run', 'Hi Speed', 'Hi Dir', 'Wind Chill', 'Heat Index', 'THW Index', 'THSW Index', 'Pressão Atm.', 'Precipitação', 'Rain Rate', 'Solar Rad', 'Solar Energy', 'Hi Solar Rad', 'UVI', 'UV Dose', 'Hi UV', 'Heat D-D', 'Cool D-D', 'In Temp', 'In Hum', 'In Dew', 'In Heat', 'In EMC', 'In Air Density', 'ET', 'Wind Samp', 'Wind TX', 'ISS Recept', 'Arc Int.']
+topo = ['Date', 'Time','Temperatura (°C)', 'Hi temp (°C)', 'Low Temp (°C)', 'Umidade (%)', 'Dew Pt. (°C)', 'Velocidade do Vento (m/s)', 'Wind Dir', 'Wind Run (m/s)', 'Hi Speed (m/s)', 'Hi Dir', 'Wind Chill', 'Heat Index', 'THW Index', 'THSW Index', 'Pressão Atm.', 'Precipitação', 'Rain Rate (mm)', 'Solar Rad', 'Solar Energy', 'Hi Solar Rad', 'UVI', 'UV Dose', 'Hi UV', 'Heat D-D', 'Cool D-D', 'In Temp', 'In Hum', 'In Dew', 'In Heat', 'In EMC', 'In Air Density', 'ET', 'Wind Samp', 'Wind TX', 'ISS Recept', 'Arc Int.']
 dados = '1213.txt'
-#dados='https://raw.githubusercontent.com/renangadelha12/teste12/main/dall.txt'
-#https://raw.githubusercontent.com/renangadelha12/teste/main/dall.txt
+topo2 = ['Temperatura (°C)', 'Hi temp (°C)', 'Low Temp (°C)', 'Umidade (%)', 'Dew Pt. (°C)', 'Velocidade do Vento (m/s)', 'Wind Dir ', 'Wind Run (m/s)', 'Hi Speed (m/s)', 'Hi Dir', 'Wind Chill', 'Heat Index', 'THW Index', 'THSW Index', 'Pressão Atm.', 'Precipitação', 'Rain Rate (mm)', 'Solar Rad', 'Solar Energy', 'Hi Solar Rad', 'UVI', 'UV Dose', 'Hi UV', 'Heat D-D', 'Cool D-D', 'In Temp', 'In Hum', 'In Dew', 'In Heat', 'In EMC', 'In Air Density', 'ET', 'Wind Samp', 'Wind TX', 'ISS Recept', 'Arc Int.']
 
 davis = pd.read_csv(dados, sep='\t', dtype={'Temp OUT': float, 'Out Hum': float, 'Rain': float, 'UV Index': float, 'UV Dose': float, 'Bar': float, 'Dew Pt.': float, 'Heat Index': float}, na_values=['---', '------'], header=1, names=topo)
+davis['Dat']=davis['Date']
 davis["Date"] = pd.to_datetime(davis["Date"])
 davis['Dia'] = davis['Date'].dt.day
 davis['Year'] = davis['Date'].dt.year
@@ -61,26 +61,27 @@ davis['Mês'] = davis['Date'].dt.month
 anos_lista = list(davis['Year'].unique())
 meses_lista = list(davis['Mês'].unique())
 anos_lista_analise_1=anos_lista
-dias_lista=list(davis['Date'].unique())
-variaveis=list(topo)
+dias_lista=list(davis['Dat'].unique())
+variaveis=list(topo2)
 variaveis_analise1=variaveis
 tipos_de_analise=list(['Média','Máximos','Minimos'])
+selecao_1=list(['Gráfico','Tabela'])
 # Usando HTML para estilizar o título
 st.markdown('<h1 style="color:orange">Dashboard de Dados Meteorológicos - Vantage Pro 2</h1>', unsafe_allow_html=True)
 
 
 # Seleção de ano , mês e dia
 #mes_selecionado = st.selectbox('Selecione o mês', meses_lista, index=0)
-dia_selecionado = st.selectbox('Selecione o dia',dias_lista,index=0)
-variavel_grafico=st.selectbox('Selecione a variável para graficar',variaveis,index=0)
+dia_selecionado = st.selectbox('Selecione o dia',dias_lista,index=2)
+variavel_grafico=st.selectbox('Selecione a variável para graficar',variaveis,index=2)
 
 
 # Filtrando os dados
 #davis_selecionado = davis[(davis['Year'] == ano_selecionado) & (davis['Mês'] == mes_selecionado)]
 davis_selecionado1 = davis[davis['Date'] == dia_selecionado]
 davis_selecionado1.loc[:, 'Hora'] = davis_selecionado1['Time']
-#davis_selecionado1.loc[:, 'Hi Dir'] = davis_selecionado1['Hi Dir'].replace({'N': 0, 'NNE': 22.5, 'NE': 45.0, 'ENE': 67.5, 'E': 90.0, 'ESE': 112.5, 'SE': 135.0, 'SSE': 157.5, 'S': 180, 'SSW': 202.5, 'SW': 225.0, 'WSW': 247.5, 'W': 270, 'WNW': 292.5, 'NW': 315, 'NNW': 337.5})
-#davis_selecionado1.loc[:, 'Wind Dir'] = davis_selecionado1['Wind Dir'].replace({'N': 0, 'NNE': 22.5, 'NE': 45.0, 'ENE': 67.5, 'E': 90.0, 'ESE': 112.5, 'SE': 135.0, 'SSE': 157.5, 'S': 180, 'SSW': 202.5, 'SW': 225.0, 'WSW': 247.5, 'W': 270, 'WNW': 292.5, 'NW': 315, 'NNW': 337.5})
+davis_selecionado1.loc[:, 'Hi Dir'] = davis_selecionado1['Hi Dir'].replace({'N': 0, 'NNE': 22.5, 'NE': 45.0, 'ENE': 67.5, 'E': 90.0, 'ESE': 112.5, 'SE': 135.0, 'SSE': 157.5, 'S': 180, 'SSW': 202.5, 'SW': 225.0, 'WSW': 247.5, 'W': 270, 'WNW': 292.5, 'NW': 315, 'NNW': 337.5})
+davis_selecionado1.loc[:, 'Wind Dir'] = davis_selecionado1['Wind Dir'].replace({'N': 0, 'NNE': 22.5, 'NE': 45.0, 'ENE': 67.5, 'E': 90.0, 'ESE': 112.5, 'SE': 135.0, 'SSE': 157.5, 'S': 180, 'SSW': 202.5, 'SW': 225.0, 'WSW': 247.5, 'W': 270, 'WNW': 292.5, 'NW': 315, 'NNW': 337.5})
 davis_selecionado1.loc[:, 'Hora'] = pd.to_datetime(davis_selecionado1['Hora'], format='%H:%M').dt.strftime('%H:%M')
 
 
@@ -89,7 +90,7 @@ chart = alt.Chart(davis_selecionado1).mark_line().encode(
     y=alt.Y(f'{variavel_grafico}:Q', title=f'{variavel_grafico}'), 
     tooltip=['Hora', 'Temperatura:Q']
 ).properties(
-    title='Temperatura ao longo do dia selecionado'
+    title=f'{variavel_grafico} ao longo do dia selecionado'
 )
 
 st.altair_chart(chart, use_container_width=True)
@@ -116,17 +117,93 @@ chart_rain = alt.Chart(precipitacao_por_mes).mark_arc().encode(
 
 # Exibindo o gráfico no Streamlit
 st.altair_chart(chart_rain, use_container_width=True)
-
 #aqui nos vamos fazer a parte que bota o gráfico de observações anuais  - analise 1
-ano_analise1 = st.selectbox('Selecione o ano', anos_lista_analise_1, index=0, key='ano_analise1')
-variavel_analise1 = st.selectbox('Selecione a variável para graficar', variaveis_analise1, index=0, key='variavel_analise1')
-parametro_analise = st.selectbox('Selecione a como deseje realizar a plotagem dos dados:', tipos_de_analise, index=0, key='parametro_analise')
+anual_mensal=st.radio('Escolha entre análises anuais e mensais',['Anual','Mensal'])
+if anual_mensal == 'Anual':
+    ano_analise1 = st.selectbox('Selecione o ano', anos_lista_analise_1, index=0, key='ano_analise1')
+if anual_mensal == 'Mensal':
+    ano_analise1 = st.selectbox('Selecione o ano', anos_lista_analise_1, index=0, key='ano_analise1')
+    mes_analise=st.radio('Escolha um mês',['1','2','3','4','5','6','7','8','9','10','11','12'])
+    mes_analise = int(mes_analise)
+
+med_min_max=st.radio('Escolha uma opção',['Média','Mínimo','Máximo'])
+
+n_variaveis=st.radio('Quantas variaveis para analisar',[1,2,3,4])
+conjunto_variaveis = []
+
+for i in range(n_variaveis):
+    variavel = st.selectbox(f'Selecione a variável {i+1}', variaveis_analise1, key=f'var_{i}')
+    conjunto_variaveis.append(variavel)
+
+if med_min_max == 'Média':
+    med_min_max = 'mean'
+
+elif med_min_max == 'Mínimo':
+    med_min_max = 'min'
+
+elif med_min_max == 'Máximo':
+    med_min_max = 'max'
+
+# Criar um seletor de opções com radio buttons
+tab_or_graf = st.radio('Escolha uma opção:', ['Tabela', 'Gráfico'])
+
+davis_analise1=davis[davis['Year'] == ano_analise1]
 
 
+davis_analise1 = davis_analise1[['Year', 'Dat', 'Mês', 'Time', 'Dia'] + conjunto_variaveis]
+davis_analise1.loc[:, 'Hora'] = davis_analise1['Time']
+davis_analise1.loc[:, 'Hora'] = pd.to_datetime(davis_analise1['Hora'], format='%H:%M').dt.strftime('%H:%M')
+
+#Determinar como vai ser o df da analise
+# Aplicar a função estatística correta
+davis_analise1 = davis_analise1.groupby('Mês')[conjunto_variaveis].agg(med_min_max).reset_index()
+if anual_mensal == 'Mensal':
+    davis_analise1 = davis[(davis['Year'] == ano_analise1) & (davis['Mês'] == int(mes_analise))]
+    davis_analise1 = davis_analise1.groupby('Dia')[conjunto_variaveis].agg(med_min_max).reset_index()
+
+if tab_or_graf == 'Tabela':
+
+    st.write(davis_analise1)
+
+elif tab_or_graf == 'Gráfico':
+    variavel_analise1=conjunto_variaveis[0]     
+    eixo_x = 'Mês' if anual_mensal == 'Anual' else 'Dia'
+# Criar gráfico corrigido
+    line_or_scatter=st.radio('Tipo do Gráfico',['Linha','Dispersão'])
+    if anual_mensal == 'Mensal':
+        titulo_grafico = f'{variavel_analise1} ({med_min_max}) ao longo do período selecionado: {mes_analise1}-{ano_analise1}'
+    elif anual_mensal == 'Anual':
+        titulo_grafico = f'{variavel_analise1} ({med_min_max}) ao longo do período selecionado: {ano_analise1}'
+
+    if line_or_scatter == 'Linha':
+        chart2 = alt.Chart(davis_analise1).mark_line().encode(
+        x=alt.X(f'{eixo_x}:O', title=eixo_x),
+        y=alt.Y(
+            f'{variavel_analise1}:Q', 
+            title=f'{variavel_analise1}',  
+            scale=alt.Scale(domain=[davis_analise1[variavel_analise1].min(), davis_analise1[variavel_analise1].max()])
+        )
+        ).properties(
+        title= titulo_grafico
+        )
+        st.altair_chart(chart2, use_container_width=True)
 
 
+    if line_or_scatter =='Dispersão':
+    
+        chart2 = alt.Chart(davis_analise1).mark_square().encode(
+        x=alt.X(f'{eixo_x}:O', title=eixo_x),
+        y=alt.Y(
+            f'{variavel_analise1}:Q', 
+            title=f'{variavel_analise1}',  
+            scale=alt.Scale(domain=[davis_analise1[variavel_analise1].min(), davis_analise1[variavel_analise1].max()])
+        )
+        ).properties(
+        title=titulo_grafico
+        )
+        st.altair_chart(chart2, use_container_width=True)
 
-# Exibindo os dados em formato de tabela
-#st.write(davis_selecionado1[['Hora', 'Temperatura', 'Precipitação','Wind Dir','Velocidade do Vento']])
+
+    
 
 
